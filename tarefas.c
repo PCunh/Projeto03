c6ccb0a7cf7460408af51c9245230ac132fea984
Felipeszzz <160777929+Felipeszzz@users.noreply.github.com>
2024-04-19 20:00:27 +0000

    lista as informacoes do usuario, e tambem adicionamos email e telefone

--- tarefas.c
+++ tarefas.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 #include "tarefas.h"
+
+void extermina_linha(char *str) {
+    int len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
+void clearBuffer(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF){
+    }
+}
 
 // Função para adicionar um novo contato à lista
 void adicionarContato(struct Contato lista[], int *numContatos) {
     if (*numContatos < MAX_CONTATOS) {
         printf("Digite o seu nome: ");
         scanf("%s", lista[*numContatos].nome);
+        extermina_linha(lista[*numContatos].nome);
+        clearBuffer();
+
         printf("Digite o seu sobrenome: ");
         scanf("%s", lista[*numContatos].sobrenome);
+        extermina_linha(lista[*numContatos].sobrenome);
+        clearBuffer();
+
+        printf("Digite o seu email: ");
+        scanf("%s", lista[*numContatos].email);
+        extermina_linha(lista[*numContatos].email);
+        clearBuffer();
+
+        printf("Digite o seu telefone: ");
+        scanf("%s", lista[*numContatos].telefone);
+        extermina_linha(lista[*numContatos].telefone);
+        clearBuffer();
+
         (*numContatos)++;
     } else {
         printf("Lista de contatos cheia!\n");
@@ -17,8 +47,10 @@ void adicionarContato(struct Contato lista[], int *numContatos) {
 // Função para exibir todos os contatos na lista
 void ListarContatos(struct Contato lista[], int numContatos) {
     printf("Lista de Contatos:\n");
-    printf("\n");
     for (int i = 0; i < numContatos; i++) {
-        printf("%s %s\n", lista[i].nome, lista[i].sobrenome);
+        printf("NOME: %s\n", lista[i].nome);
+        printf("SOBRENOME: %s\n", lista[i].sobrenome);
+        printf("EMAIL: %s\n", lista[i].email);
+        printf("TELEFONE: %s\n", lista[i].telefone);
     }
 }
